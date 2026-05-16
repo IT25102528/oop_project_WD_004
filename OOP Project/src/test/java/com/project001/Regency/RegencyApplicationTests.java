074b0cca7d8c2f68607129e9be7028939a09c19c
IT25102528 <it25102528@my.sliit.lk>
2026-05-16 21:44:35 +0530

    create file structure

---
+++ OOP Project/src/test/java/com/project001/Regency/RegencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project001.Regency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
