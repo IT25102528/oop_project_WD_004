b33b8342239512549442e29d7b8a656d8944d4ba
IT25102528 <it25102528@my.sliit.lk>
2026-05-16 21:50:31 +0530

    user repository

---
+++ OOP Project/src/main/java/com/project001/Regency/repository/UsersRepo.java
@@ -0,0 +1,13 @@
+package com.project001.Regency.repository;
+
+import com.project001.Regency.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepo extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+
+}
