bdbeed2eb3e3a82e72d57c677a5e72a768ce063c
IT25102512 <it25102512@my.sliit.lk>
2026-05-16 23:11:26 +0530

    room entity

---
+++ OOP Project/src/main/java/com/project001/Regency/model/Room.java
@@ -0,0 +1,96 @@
+package com.project001.Regency.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String roomNumber;
+    private String roomType;
+    private double pricePerNight;
+    private int capacity;
+    private boolean available;
+    private String description;
+    private String acType;
+
+    public Room() {
+    }
+
+    public Room(String roomNumber, String roomType, double pricePerNight, int capacity, boolean available, String description,String acType) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.pricePerNight = pricePerNight;
+        this.capacity = capacity;
+        this.available = available;
+        this.description = description;
+        this.acType = acType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAcType() {
+        return acType;
+    }
+
+    public void setAcType(String acType) {
+        this.acType = acType;
+    }
+}
+
