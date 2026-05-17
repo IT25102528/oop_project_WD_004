3cc79effc9a3e6456f734fee61b3d317f6927aa5
it25201099 <it25201099@my.sliit.lk>
2026-05-17 16:41:37 +0530

    Feedback entity

---
+++ OOP Project/src/main/java/com/project001/Regency/model/Feedback.java
@@ -0,0 +1,91 @@
+package com.project001.Regency.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "feedbacks")
+public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private String userName;
+    private String email;
+    private String message;
+    private int rating;
+
+    private LocalDateTime createdAt;
+
+
+    // constructors
+    public Feedback() {}
+
+    public Feedback(Long userId, String userName, String email, String message,int rating) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.message = message;
+        this.rating = rating;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
