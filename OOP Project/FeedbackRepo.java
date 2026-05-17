b06aa8a49f54b7780b77c649258c5c1052897e4c
IT25200808 <IT25200808@my.sliit.lk>
2026-05-17 17:00:31 +0530

    Delete OOP Project/FeedbackRepo.java

--- OOP Project/FeedbackRepo.java
+++
@@ -1,7 +0,0 @@
-package com.project001.Regency.repository;
-
-import com.project001.Regency.model.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepo extends JpaRepository<Feedback, Long> {
-}
